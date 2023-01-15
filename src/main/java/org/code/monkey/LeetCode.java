0d9c8f60973054a332a97ac9bc63c390a32c4ccd
bitterbit <35038882+shubhDcoder@users.noreply.github.com>
2023-01-15 18:37:32 +0530

    first commit

---
+++ src/main/java/org/code/monkey/LeetCode.java
@@ -0,0 +1,7 @@
+package org.code.monkey;
+
+public class LeetCode {
+    public static void main(String[] args) {
+        System.out.println("A dummy leetcode starter file");
+    }
+}
---
+++ src/test/java/org/code/monkey/LeetCodeTest.java
@@ -0,0 +1,14 @@
+package org.code.monkey;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeetCodeTest {
+
+    @Test
+    public void testMain(){
+        assertTrue(true);
+    }
+
+}
